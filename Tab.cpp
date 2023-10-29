1ea6fcfaf3e47446a751cf885bed86a0a6f47e51
Tuan <nguyenhahongtuan@gmail.com>
2023-10-29 17:08:48 +0700

    1 tab: many pages, 1 window: many tabs.

--- Tab.cpp
+++ Tab.cpp
@@ -6,119 +6,203 @@ struct NodePage
     string url;
     NodePage *next;
     NodePage *prev;
+    NodePage(string val)
+    {
+        url = val;
+        next = nullptr;
+        prev = nullptr;
+    }
 };
-struct ListPage
-{
-    NodePage *head;
-    NodePage *tail;
-};
-class Page
-{
-    public:
-        ListPage listPage;
 
-        Page()
-        {
-            this->listPage.head = this->listPage.tail = NULL;
+class Tab {
+public:
+    NodePage* head;
+    NodePage* tail;
+    NodePage* currentPage;
+
+    Tab() {
+        head = nullptr;
+        tail = nullptr;
+    }
+
+    void addTail(string address) {
+        NodePage* newNode = new NodePage(address);
+        currentPage = newNode;
+        if (tail == nullptr) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode->prev = tail;
+            tail->next = newNode;
+            tail = newNode;
         }
+    }
 
-        void add(string address)
-        {
-            NodePage *temp = new NodePage();
-            temp->url = address;
-            temp->next == NULL;
-            temp->prev == NULL;
 
-            if (this->listPage.head == NULL)
-            {
-                this->listPage.head = this->listPage.tail = temp;
-            }
-            else
-            {
-                this->listPage.tail->next = temp;
-                temp->prev = this->listPage.tail;
-                this->listPage.tail = temp;
-            }
-        } 
+    void deleteHead() {
+        if (head != nullptr) {
+            NodePage* temp = head;
+            head = head->next;
 
-        void deleteHead()
-        {
-            if (this->listPage.head == NULL)
-            {
-                cout << "There is no page to delete." << endl;
-                return;
+            if (head != nullptr) {
+                head->prev = nullptr;
+            } else {
+                // If head becomes nullptr, update tail as well
+                tail = nullptr;
             }
 
-            NodePage *temp = this->listPage.head;
-            this->listPage.head->next->prev = NULL;
-            this->listPage.head = this->listPage.head->next;
+            currentPage = head;
             delete temp;
-
-            if (this->listPage.head == NULL)
-            {
-                this->listPage.tail = NULL;
-            }
         }
-
-        void deleteTail()
+        else
         {
-            if (this->listPage.head == NULL)
-            {
-                cout << "There is no page to delete." << endl;
-                return;
-            }
-
-            if (this->listPage.head->next == NULL)
-            {
-                deleteHead();
-                return;
-            }
-
-            for (NodePage *p = this->listPage.head; p != NULL; p = p->next)
-            {
-                if (p->next == this->listPage.tail)
-                {
-                    this->listPage.tail->prev = NULL;
-                    delete this->listPage.tail;
-                    p->next = NULL;
-                    this->listPage.tail = p;
-                    return;
-                }
-            }
+            cout << "This TAB is empty." << endl;
         }
+    }
 
-        void print()
+    void deleteTail() {
+        if (tail != nullptr) {
+            NodePage* temp = tail;
+            tail = tail->prev;
+
+            if (tail != nullptr) {
+                tail->next = nullptr;
+            } else {
+                // If tail becomes nullptr, update head as well
+                head = nullptr;
+            }
+            currentPage = tail;
+            delete temp;
+        }
+        else
         {
-            for (NodePage *p = this->listPage.head; p != NULL; p = p->next)
-            {
-                cout << p->url << endl;
-            }
+            cout << "This TAB is empty." << endl;
         }
+    }
+
+    void display() {
+        NodePage* current = head;
+        while (current != nullptr) {
+            cout << current->url << " " << endl;
+            current = current->next;
+        }
+        cout << endl;
+    }
 };
+
 struct NodeTab
 {
-    ListPage listPage;
+    Tab tab;
     NodeTab *next;
     NodeTab *prev;
+    NodeTab(Tab val)
+    {
+        tab = val;
+        next = nullptr;
+        prev = nullptr;
+    }
 };
-struct ListTab
-{
-    NodeTab *head;
-    NodeTab *tail;
+
+class Window {
+public:
+    NodeTab* head;
+    NodeTab* tail;
+    NodeTab* currentTab;
+
+    Window() {
+        head = nullptr;
+        tail = nullptr;
+    }
+
+    void addTail(Tab tab) {
+        NodeTab* newNode = new NodeTab(tab);
+        currentTab = newNode;
+        if (tail == nullptr) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode->prev = tail;
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+
+    void deleteHead() {
+        if (head != nullptr) {
+            NodeTab* temp = head;
+            head = head->next;
+
+            if (head != nullptr) {
+                head->prev = nullptr;
+            } else {
+                // If head becomes nullptr, update tail as well
+                tail = nullptr;
+            }
+
+            currentTab = head;
+            delete temp;
+        }
+        else
+        {
+            cout << "This TAB is empty." << endl;
+        }
+    }
+
+    void deleteTail() {
+        if (tail != nullptr) {
+            NodeTab* temp = tail;
+            tail = tail->prev;
+
+            if (tail != nullptr) {
+                tail->next = nullptr;
+            } else {
+                // If tail becomes nullptr, update head as well
+                head = nullptr;
+            }
+            currentTab = tail;
+            delete temp;
+        }
+        else
+        {
+            cout << "This TAB is empty." << endl;
+        }
+    }
+
+    void display() {
+        NodeTab* current = head;
+        while (current != nullptr) {
+            current->tab.display();
+            current = current->next;
+        }
+        cout << endl;
+    }
 };
 
 int main()
 {
-    Page page1;
-    page1.add("www.google.com");
-    page1.add("www.youtube.com");
-    page1.add("www.facebook.com");
+    Tab tab1;
+    tab1.addTail("www.google.com");
+    cout << tab1.currentPage << endl;
+    tab1.addTail("www.youtube.com");
+    cout << tab1.currentPage << endl;
+    tab1.addTail("www.facebook.com");
+    cout << tab1.currentPage << endl;
+    tab1.display();
 
-    page1.print();
+    Tab tab2;
+    tab2.addTail("www.google.com");
+    cout << tab2.currentPage << endl;
+    tab2.addTail("www.youtube.com");
+    cout << tab2.currentPage << endl;
+    tab2.addTail("www.facebook.com");
+    cout << tab2.currentPage << endl;
+    tab2.display();
 
-    page1.deleteHead();
-    page1.deleteTail();
-    page1.print();
+    Window w1;
+    w1.addTail(tab1);
+    w1.addTail(tab2);
+    w1.display();
 
     return 0;
 }
